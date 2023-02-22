3f84cc1d2065903e67db7eee53cf7df36587d11b
Oscar Veldman <oveldman@gmail.com>
2023-02-22 23:49:53 +0100

    add rest api to rest

--- DragonMaster/MadWorld.Backend/madworld_api/src/main.rs
+++ DragonMaster/MadWorld.Backend/madworld_api/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[rocket::main]
+async fn main() {
+    let _ = rocket::build().mount("/", routes![index]).launch().await;
 }
